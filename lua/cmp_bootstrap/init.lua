307407d5f8af31bfc980684846fc926fad7573c6
Ram Bhosale <rambhosale9@gmail.com>
2022-03-18 18:04:54 +0530

    update: change the keyword pattern and cmp response

--- lua/cmp_bootstrap/init.lua
+++ lua/cmp_bootstrap/init.lua
@@ -1,5 +1,6 @@
 local source = {}
 
+local cmp = require("cmp")
 local items = require("cmp_bootstrap/bootstrap-v5")
 
 source.new = function()
@@ -11,19 +12,27 @@ source.get_trigger_characters = function()
 end
 
 source.get_keyword_pattern = function()
-  return "[^[:blank:]]*"
+  return [[\([^"' ~\.\s]\)*]]
 end
 
 source.complete = function(self, request, callback)
 
-    print(vim.inspect(request))
   if not vim.regex(self.get_keyword_pattern() .. "$"):match_str(request.context.cursor_before_line) then
     return callback()
   end
-  if not self.items then
-    self.items = items
+
+  local generatedClasses = {}
+  for i, class in pairs(items) do
+    table.insert(generatedClasses, {
+      word=class,
+      label=class,
+      insertText=class,
+      filterText=class,
+      kind = cmp.lsp.CompletionItemKind.Value,
+    })
   end
-  callback(self.items)
+
+  callback(generatedClasses)
 end
 
 return source
